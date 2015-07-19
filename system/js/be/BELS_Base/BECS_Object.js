a0b6de96efabd92360431b5dff752be5252826c6
craig <craig@here.org>
2015-07-19 10:14:47 -0700

    update license

--- system/js/be/BELS_Base/BECS_Object.js
+++ system/js/be/BELS_Base/BECS_Object.js
@@ -1,4 +1,10 @@
-
+// Copyright 2015 Craig Welch
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
 var be_BELS_Base_BECS_Object = function() { }
 
 be_BELS_Base_BECS_Object.prototype.becs_insts = function() { }
--- system/jv/be/BELS_Base/BECS_Object.java
+++ system/jv/be/BELS_Base/BECS_Object.java
@@ -1,3 +1,11 @@
+// Copyright 2015 Craig Welch
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 package be.BELS_Base;
 
 //Native / generated Object, rather than this class is the untyped/var type to avoid unnecessary casting
