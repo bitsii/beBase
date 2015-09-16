9c6d9b1af281d1255873b0ea53fd768c5fbf0a4e
craig <craig@here.org>
2015-09-15 18:28:45 -0700

    add win boots

---
+++ scripts/genboot5jvmswin.sh
@@ -0,0 +1,15 @@
+java -classpath target5/BEL_system_be_jv.jar:target5/BEL_4_Base_be_jv.jar be.BEL_4_Base.BEL_4_Base --buildFile build/buildbuild.txt --deployPath deploy5 --buildPath target5 --emitLang jv --outputPlatform mswin
+
+find system -name "*.class" -exec rm {} \;
+find target5 -name "*.class" -exec rm {} \;
+
+mkdir -p boot5
+rm -f boot5/BEL_system_be_jv_mswin.zip
+cd system
+zip -r ../boot5/BEL_system_be_jv_mswin.zip jv
+cd ..
+
+rm -f boot5/BEL_4_Base_be_jv_mswin.zip
+cd target5/Base/target
+zip -r ../../../boot5/BEL_4_Base_be_jv_mswin.zip jv
+cd ../../..
---
+++ scripts/genboot5mcsmswin.sh
@@ -0,0 +1,12 @@
+mono --debug target5/BEL_4_Base_mcs.exe --buildFile build/buildbuild.txt --deployPath deploy5 --buildPath target5 --emitLang cs --outputPlatform mswin
+
+mkdir -p boot5
+rm -f boot5/BEL_system_be_mcs_mswin.zip
+cd system
+zip -r ../boot5/BEL_system_be_mcs_mswin.zip cs
+cd ..
+
+rm -f boot5/BEL_4_Base_be_mcs_mswin.zip
+cd target5/Base/target
+zip -r ../../../boot5/BEL_4_Base_be_mcs_mswin.zip cs
+cd ../../..
