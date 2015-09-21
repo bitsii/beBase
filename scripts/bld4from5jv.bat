2d5960f07906267d7515e96194b377b28051d4f1
craig <craig@here>
2015-09-20 22:02:29 -0700

    fix jv boot builds for mswin

--- scripts/bld4from5jv.bat
+++ scripts/bld4from5jv.bat
@@ -1,4 +1,18 @@
-java -classpath target5\Base\target\jv;system\jv be.BEL_4_Base.BEL_4_Base --buildFile build\buildbuild.txt --deployPath deploy4 --buildPath target4 --emitLang jv
+java -classpath target5\BEL_system_be_jv.jar;target5\BEL_4_Base_be_jv.jar be.BEL_4_Base.BEL_4_Base --buildFile build\buildbuild.txt --deployPath deploy4 --buildPath target4 --emitLang jv
 javac system\jv\be\BELS_Base\*.java target4\Base\target\jv\be\BEL_4_Base\*.java
 
+del target4\BEL_system_be_jv.jar
+cd system\jv
+jar -cf ..\..\target4\BEL_system_be_jv.jar .
+cd ..\..
 
+del target4\BEL_4_Base_be_jv.jar
+cd target4\Base\target\jv
+jar -cf ..\..\..\BEL_4_Base_be_jv.jar .
+cd ..\..\..\..
+
+cd system
+del /s *.class
+cd ..\target4
+del /s *.class
+cd ..
--- scripts/boot5jvmswin.bat
+++ scripts/boot5jvmswin.bat
@@ -1,4 +1,13 @@
-java -classpath target4\BEL_system_be_jv.jar;target4\BEL_4_Base_be_jv.jar be.BEL_4_Base.BEL_4_Base --buildFile build\buildbuild.txt --deployPath deploy5 --buildPath target5 --emitLang jv
+mkdir system
+cd system
+unzip -o ..\boot5\BEL_system_be_jv_mswin.zip
+cd ..
+
+mkdir target5\Base\target
+cd target5\Base\target
+unzip -o ..\..\..\boot5\BEL_4_Base_be_jv_mswin.zip
+cd ..\..\..
+
 javac system\jv\be\BELS_Base\*.java target5\Base\target\jv\be\BEL_4_Base\*.java
 
 del target5\BEL_system_be_jv.jar
@@ -16,3 +25,5 @@ del /s *.class
 cd ..\target5
 del /s *.class
 cd ..
+
+call scripts\bld4from5jv.bat
