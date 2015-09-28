95b562d44ce16292be29ef24ebdbc9fe73bfe782
craig <craig@here.org>
2015-09-27 22:10:11 -0700

    test and cycle going

--- scripts/cyclejv5.bat
+++ scripts/cyclejv5.bat
@@ -1,4 +1,4 @@
-java -classpath target5\Base\target\jv;system\jv be.BEL_4_Base.BEL_4_Base --buildFile build\buildbuild.txt --deployPath cycle\deploy0 --buildPath cycle\target0 --emitLang jv
+java -classpath target5/BEL_system_be_jv.jar;target5/BEL_4_Base_be_jv.jar be.BEL_4_Base.BEL_4_Base --buildFile build\buildbuild.txt --deployPath cycle\deploy0 --buildPath cycle\target0 --emitLang jv
 javac system\jv\be\BELS_Base\*.java cycle\target0\Base\target\jv\be\BEL_4_Base\*.java
 
 java -classpath cycle\target0\Base\target\jv;system\jv be.BEL_4_Base.BEL_4_Base --buildFile build\buildbuild.txt --deployPath cycle\deploy1 --buildPath cycle\target1 --emitLang jv
