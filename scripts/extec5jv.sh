6e32d0accd9c76659f0187b2d38765fdee2887bf
craig <craig@here>
2015-10-04 13:52:50 -0700

    update plans, rm unneeded rm

--- scripts/extec5jv.sh
+++ scripts/extec5jv.sh
@@ -5,5 +5,3 @@ rm -rf targetEc/Base/target/jv
 java -classpath target5/BEL_system_be_jv.jar:target5/BEL_4_Base_be_jv.jar be.BEL_4_Base.BEL_4_Base --buildFile build/extendedEc.txt --emitLang jv
 javac system/jv/be/BELS_Base/*.java targetEc/Base/target/jv/be/BEL_4_Base/*.java
 java -classpath targetEc/Base/target/jv:system/jv be.BEL_4_Base.BEL_4_Base $*
-
-find system -name "*.class" -exec rm {} \;
