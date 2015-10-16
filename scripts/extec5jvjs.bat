778673e62b32b1dec794ac822161cf6e7ffbb40f
craig <craig@here>
2015-10-15 21:43:23 -0700

    fix extec5jvjs, update mangling issue

--- scripts/extec5jvjs.bat
+++ scripts/extec5jvjs.bat
@@ -1,5 +1,4 @@
 
-java -classpath target5\Base\target\jv;system\jv be.BEL_4_Base.BEL_4_Base --buildFile build\extendedEc.txt --emitLang js
+java -classpath target5/BEL_system_be_jv.jar;target5/BEL_4_Base_be_jv.jar be.BEL_4_Base.BEL_4_Base --buildFile build\extendedEc.txt --emitLang js
 
 node targetEc/Base/target/js/be/BEL_4_Base/BEL_4_Base.js %*
-
