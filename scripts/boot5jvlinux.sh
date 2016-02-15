645779db6a6df50162d4cc66d03a3f246c14cdce
Craig <Craig>
2016-02-14 19:40:58 -0800

    add linux

---
+++ scripts/boot5jvlinux.sh
@@ -0,0 +1,27 @@
+
+mkdir -p system
+cd system
+unzip -o ../boot5/BEL_system_be_jv_linux.zip
+cd ..
+
+mkdir -p target5/Base/target
+cd target5/Base/target
+unzip -o ../../../boot5/BEL_4_Base_be_jv_linux.zip
+cd ../../..
+
+javac system/jv/be/BELS_Base/*.java target5/Base/target/jv/be/BEL_4_Base/*.java
+
+rm -f target5/BEL_system_be_jv.jar
+cd system/jv
+jar -cf ../../target5/BEL_system_be_jv.jar .
+cd ../..
+
+rm -f target5/BEL_4_Base_be_jv.jar
+cd target5/Base/target/jv
+jar -cf ../../../BEL_4_Base_be_jv.jar .
+cd ../../../..
+
+find system -name "*.class" -exec rm {} \;
+find target5 -name "*.class" -exec rm {} \;
+
+./scripts/bld4from5jv.sh
