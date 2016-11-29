9d0b32c8acf39acb10904c1175f240a86753c920
Craig <Craig>
2016-11-28 17:27:13 -0800

    .sh working in mingw o windows for extec

--- scripts/extec5jv.sh
+++ scripts/extec5jv.sh
@@ -2,14 +2,28 @@
 
 rm -rf targetEc/Base/target/jv
 
-java -classpath target5/BEL_system_be_jv.jar:target5/BEL_4_Base_be_jv.jar be.BEL_4_Base.BEL_4_Base --buildFile build/extendedEc.txt --emitLang jv
+export CLASSPATH=target5/*
+
+java be.BEL_4_Base --buildFile build/extendedEc.txt --emitLang jv
 
 lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
 
-javac system/jv/be/BELS_Base/*.java targetEc/Base/target/jv/be/BEL_4_Base/*.java
+javac system/jv/be/*.java targetEc/Base/target/jv/be/*.java
 
 lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
 
-java -classpath targetEc/Base/target/jv:system/jv be.BEL_4_Base.BEL_4_Base $*
+rm -f targetEc/BEL_system_be_jv.jar
+cd system/jv
+jar -cf ../../targetEc/BEL_system_be_jv.jar .
+cd ../..
+
+rm -f targetEc/BEL_4_Base_be_jv.jar
+cd targetEc/Base/target/jv
+jar -cf ../../../BEL_4_Base_be_jv.jar .
+cd ../../../..
+
+export CLASSPATH=targetEc/*
+
+java be.BEL_4_Base $*
 
 lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
