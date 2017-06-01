0e3c38f96e6fb2d2723df337ad8ae528c562ae7e
Craig <Craig>
2017-05-31 23:43:02 -0700

    bets

---
+++ system/cs/be/BETS_Object.cs
@@ -0,0 +1,30 @@
+// Copyright 2015 Craig Welch
+//
+// Licensed under the MIT license. See LICENSE.txt file in the project root 
+// for full license information.
+
+namespace be {
+
+using System;
+using System.Collections.Generic;
+using be;
+
+public class BETS_Object {
+    
+  public Dictionary<string, bool> bevs_methodNames;
+  
+  public void bems_buildMethodNames(string[] names) {
+    bevs_methodNames = new Dictionary<string, bool>();
+    for (int i = 0;i < names.Length;i++) {
+       bevs_methodNames[names[i]] = true;
+    }
+  }
+  
+  public BEC_2_6_6_SystemObject bems_createInstance() {
+    return null;
+  }
+
+}
+
+}
+
---
+++ system/jv/be/BETS_Object.java
@@ -0,0 +1,31 @@
+// Copyright 2015 Craig Welch
+//
+// Licensed under the MIT license. See LICENSE.txt file in the project root 
+// for full license information.
+
+package be;
+
+import java.util.*;
+
+//Native / generated Object, rather than this class is the untyped/var type to avoid unnecessary casting
+
+import be.BEC_2_6_6_SystemObject;
+
+public class BETS_Object {
+  
+  public static Map<String, Boolean> bevs_methodNames;
+  
+  public void bems_buildMethodNames(String[] names) {
+    bevs_methodNames = new HashMap<String, Boolean>();
+    for (int i = 0;i < names.length;i++) {
+       bevs_methodNames.put(names[i], true);
+    }
+  }
+  
+  public BEC_2_6_6_SystemObject bems_createInstance() {
+    return null;
+  }
+    
+}
+
+
