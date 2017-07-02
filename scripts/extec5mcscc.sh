04ba2c1e94ef6150156ace8b6260a5632896a2a8
Craig <Craig>
2017-07-02 11:29:35 -0700

    add timing to cc bld

--- scripts/extec5mcscc.sh
+++ scripts/extec5mcscc.sh
@@ -5,10 +5,20 @@ mono --debug target5/BEX_E_mcs.exe --buildFile build/extendedEc.txt --emitLang c
 
 lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
 
+CYC1=`date +%s`
+
 clang++ -o ./targetEc/BEX_E_clang.exe -ferror-limit=1 -std=c++11 ./targetEc/Base/target/cc/be/BEX_E.cpp
 
 lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
 
-#g++ -o ./targetEc/BEX_E_gcc.exe -Wfatal-errors -std=c++11 ./targetEc/Base/target/cc/be/BEX_E.cpp
+CYC2=`date +%s`
+
+expr \( $CYC2 - $CYC1 \)
+
+#g++ -O3 -o ./targetEc/BEX_E_gcc.exe -Wfatal-errors -std=c++11 ./targetEc/Base/target/cc/be/BEX_E.cpp
 
 #lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
+
+#CYC3=`date +%s`
+
+#expr \( $CYC3 - $CYC2 \)
