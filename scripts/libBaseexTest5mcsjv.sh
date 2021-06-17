25657c58650ef739299514201aec9fe7b6524f36
Craig <craig@here>
2021-06-16 22:35:18 -0700

    fix lib msw

--- scripts/libBaseexTest5mcsjv.sh
+++ scripts/libBaseexTest5mcsjv.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+una=`uname -a`
+case "$una" in
+  *Msys*)
+    export CLASSPATH="lib/ex/jv/BES_System.jar;lib/ex/jv/BEL_Base.jar;targetLibBaseexTest/Test/target/jv"
+    ;;
+  *)
+    export CLASSPATH="lib/ex/jv/BES_System.jar:lib/ex/jv/BEL_Base.jar:targetLibBaseexTest/Test/target/jv"
+    ;;
+esac
+
 rm -rf targetLibBaseexTest
 
 time mono --debug target5/BEX_E_mcs.exe source/base/Uses.be --buildFile build/libBaseexTest.txt -loadSyns=lib/ex/jv/BEL_Base.syn -loadIds=lib/ex/jv/BEL_Base -initLib=Base --emitLang jv
 
 lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
 
-javac -classpath lib/ex/jv/BES_System.jar:lib/ex/jv/BEL_Base.jar targetLibBaseexTest/Test/target/jv/be/*.java
+javac targetLibBaseexTest/Test/target/jv/be/*.java
 
 lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
 
-java -classpath lib/ex/jv/BES_System.jar:lib/ex/jv/BEL_Base.jar:targetLibBaseexTest/Test/target/jv be.BEL_Test $*
+java be.BEL_Test $*
 
 lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
