005dab215c0e4af8dbbaf60335431069e810c699
Bitsii <Bitsii>
2021-06-23 20:54:49 -0700

    fix mac devprep

--- scripts/devprep.sh
+++ scripts/devprep.sh
@@ -41,17 +41,17 @@ if [ "$OSTYPE" == "Darwin" ]; then
   echo "Installing required additional system software"
   if ! [ -x "$(command -v brew)" ]; then
     sudo mkdir -p /usr/local
-    sudo chown -R $INSUSER /usr/local
+    sudo chown -R $USER /usr/local
     echo "Homebrew, https://brew.sh, is required and will now be installed"
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
   brew tap homebrew/cask-versions
   brew install openjdk@11
   brew link --force openjdk@11
   brew install mono
-  brew install node
   brew install git
   brew install git-gui
+  brew install node
   echo "make sure you are in the brace checked out directory"
   echo "and run ./scripts/boot5jvmacos.sh"
   echo "after it completes run ./scripts/boot5mcsmacos.sh"
