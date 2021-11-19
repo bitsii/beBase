092d880d27912dcd720dff9adc50709e158e6c49
craig <craig@here.com>
2021-11-18 19:23:07 -0800

    gc changes

--- system/cc/be/BEH_GCConf.hpp
+++ system/cc/be/BEH_GCConf.hpp
@@ -1,9 +1,9 @@
 //allocsPerGc 0-4,294,967,295 :: 10000000 >>6000000<< OKish bld, 1000000 extec, diff is 1 0
 //#define BEDCC_GCAPERGC 1000000
 //#define BEDCC_GCAPERGC 10000000
-//#define BEDCC_GCAPERGC 6666666
+#define BEDCC_GCAPERGC 6666666
 //to really test gc
-#define BEDCC_GCAPERGC 4444
+//#define BEDCC_GCAPERGC 4444
 //how many marks before a sweep
 #define BEDCC_GCMPERS 16
 //#define BEDCC_GCMPERS 4
