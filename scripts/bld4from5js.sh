893d85182039e6d06d5a77e301f778a379a04142
craig <craig@here>
2023-02-05 12:12:40 -0800

    profileing for node

--- scripts/bld4from5js.sh
+++ scripts/bld4from5js.sh
@@ -2,4 +2,8 @@
 
 time node target5/Base/target/js/be/BEL_Base.js --buildFile build/buildbuild.txt --deployPath deploy4 --buildPath target4 --emitLang js
 
+#\time --verbose
+#--prof
+#node --prof-process isolate-0x563fd88cb340-809577-v8.log > processed.txt
+
 lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
