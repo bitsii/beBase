e4d60005d86467b1f5ac499688b0ae6e9b7f7edf
craig <craig@here>
2024-02-06 23:16:12 -0800

    settle on tiny, match to emb

--- scripts/tiny5jvccgn.sh
+++ scripts/tiny5jvccgn.sh
@@ -14,18 +14,21 @@ rm -rf targets/min/Base/target/cc/be
 rm -f targets/min/BEX_E_cl.exe
 
 export CLASSPATH=target5/*
-time java -XX:-UsePerfData -XX:TieredStopAtLevel=1 -XX:+UseSerialGC be.BEL_Base -deployPath=targets/min -buildPath=targets/min --buildFile build/minBase.txt --emitLang cc --singleCC true --emitFlag ccSgc --emitFlag ccNoRtti --emitFlag noSmap --emitFlag noRfl --mainClass Test:TestHelloWorld ../beBase/source/baseTest/TestHelloWorld.be
+time java -XX:-UsePerfData -XX:TieredStopAtLevel=1 -XX:+UseSerialGC be.BEL_Base -deployPath=targets/tiny -buildPath=targets/tiny --buildFile build/tinyBase.txt --emitLang cc --singleCC true --emitFlag ccSgc --emitFlag ccNoRtti --emitFlag noSmap --emitFlag noRfl --mainClass Test:TestHelloWorld ../beBase/source/baseTest/TestHelloWorld.be
 
 lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
 
 CYC1=`date +%s`
 
-time clang++ -DBEDCC_SGC=1 -o targets/min/BEX_E_cl.exe -ferror-limit=1 -std=c++11 ./targets/min/Base/target/cc/be/BEL_Base.cpp
+export CC=g++
+export CPFLAGS="-std=c++11 -Wfatal-errors -ggdb"
+
+time g++ $CPFLAGS -DBEDCC_SGC=1 -o targets/tiny/BEX_E_gn.exe ./targets/tiny/Base/target/cc/be/BEL_Base.cpp
 
 
 lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
 
-time ./targets/min/BEX_E_cl.exe
+time ./targets/tiny/BEX_E_gn.exe
 
 #sudo apt-get install valgrind kcachegrind graphviz
 #valgrind --tool=callgrind ./targetEc/BEX_E_gn.exe
