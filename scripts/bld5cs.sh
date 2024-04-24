e64b674dbbce250bcb95fb678ecf480243fb4c35
craig <craig@here>
2024-04-23 21:24:31 -0700

    dotnet8 working extec5cs

--- scripts/bld5cs.sh
+++ scripts/bld5cs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-mono --debug target4/BEX_E_mcs.exe --buildFile build/buildbuild.txt --deployPath deploy5 --buildPath target5 --emitLang cs
+. scripts/bld4env.sh
+
+$BEBLDR --buildFile build/buildbuild.txt --deployPath deploy5 --buildPath target5 --emitLang cs
 
 lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
 
-mcs -debug:pdbonly -warn:0 -out:target5/BEX_E_mcs.exe system/cs/be/*.cs target5/Base/target/cs/be/*.cs
+#mcs -debug:pdbonly -warn:0 -out:target5/BEX_E_mcs.exe system/cs/be/*.cs target5/Base/target/cs/be/*.cs
 
 lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
--- scripts/extec5cs.sh
+++ scripts/extec5cs.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+. scripts/bld5env.sh
+
 rm -rf targetEc/Base/target/cs
-rm -f targetEc/BEX_E_mcs.exe
-time mono --debug target5/BEX_E_mcs.exe --buildFile build/extendedEc.txt --emitLang cs
+rm -f targetEc/bin
+
+$BEBLDR --buildFile build/extendedEc.txt --emitLang cs
+
 lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
-# --printAstElement Container:Map.addValue.178 --printAllAst true
-mcs -debug:pdbonly -warn:0 -out:targetEc/BEX_E_mcs.exe -warn:0 system/cs/be/*.cs targetEc/Base/target/cs/be/*.cs
+
+cp system/BEX_DN/BEX_DN.csproj targetEc/Base/target/cs/be
+cp system/cs/be/BE* targetEc/Base/target/cs/be/
+cd targetEc/Base/target/cs/be/
+
+dotnet build
 lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
-mono --debug targetEc/BEX_E_mcs.exe $*
+
+mv bin ../../../..
+cd ../../../../..
+
+./targetEc/bin/Debug/net8.0/BEX_DN
+
 lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
